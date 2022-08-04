f66932b3d146d269855a036da2f317d6cccac236
dowrave <dowrave@gmail.com>
2022-08-04 18:31:51 +0900

    수집 과정 거의 완료 - Date 만지고 수집된 데이터 검토 필요

---
+++ main1/mysql.sql
@@ -0,0 +1,19 @@
+-- 테이블 작성 쿼리 (실행 : MySQL 8.0 Command Line Client)
+
+-- 테이블 1 : reports (id, 상향 대상 기업명,보고서 올라온 날짜, 보고서를 쓴 증권사)
+CREATE TABLE reports(
+    id INT NOT NULL AUTO_INCREMENT,
+    company VARCHAR(40) NOT NULL,
+    date DATE NOT NULL,
+    written_by VARCHAR(20) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (company) REFERENCES companies(company) -- 이거 때문에 comapnies에 데이터를 먼저 넣어야 할 듯?
+    );
+
+-- 테이블 2 : companies (기업명, 코드, 사업 분야)
+CREATE TABLE companies(
+    -> company VARCHAR(40) NOT NULL,
+    -> code VARCHAR(6) UNIQUE NOT NULL, -- 코드 int로 받으면 안됨 : 0으로 시작하는 숫자들도 있음
+    -> category VARCHAR(60),
+    -> PRIMARY KEY (company)
+    -> );
